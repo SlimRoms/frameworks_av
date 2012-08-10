CyanogenMod/android_frameworks_av

audio: Temporary workaround for missing QCOM LPA
 * Temporary workaround to fix ABI

Change-Id: I7e7f44048b982ddbc843b3818861727fb473ae11

services/audioflinger/AudioPolicyService.cpp
@@ -1537,6 +1537,10 @@ static int aps_set_voice_volume(void *service, float volume, int delay_ms)
 namespace {
     struct audio_policy_service_ops aps_ops = {
         open_output           : aps_open_output,
+#ifdef WITH_QCOM_LPA
+        open_session          : NULL,
+        close_session         : NULL,
+#endif
         open_duplicate_output : aps_open_dup_output,
         close_output          : aps_close_output,
         suspend_output        : aps_suspend_output,

SlimRoms/frameworks_av

services/audioflinger/AudioPolicyService.cpp
@@ -1543,6 +1543,10 @@ static int aps_set_voice_volume(void *service, float volume, int delay_ms)
 namespace {
     struct audio_policy_service_ops aps_ops = {
         open_output           : aps_open_output,
+#ifdef QCOM_ICS_LPA_COMPAT
+        open_session          : NULL,
+        close_session         : NULL,
+#endif
         open_duplicate_output : aps_open_dup_output,
         close_output          : aps_close_output,
         suspend_output        : aps_suspend_output,