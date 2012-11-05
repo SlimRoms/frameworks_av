22c34457a1900734bc741cb5a3d3d8dd6f04d0da
beanz <beanz@marvell.com>
2012-11-05 09:51:43 +0800

    fix a race condition of MediaPlayerServce::Client::mClient
    there will be race condition between mClient.clear in MediaPlayerServce::Client::disconnect method
    and c=client->mClient in in MediaPlayerServce::Client::notify method
    
    Change-Id: I767294c4cb864c08e9c0e9f23d6d304a39016205
    Signed-off-by: beanz <beanz@marvell.com>

--- media/libmediaplayerservice/MediaPlayerService.cpp
+++ media/libmediaplayerservice/MediaPlayerService.cpp
@@ -526,8 +526,8 @@ void MediaPlayerService::Client::disconnect()
     {
         Mutex::Autolock l(mLock);
         p = mPlayer;
+        mClient.clear();
     }
-    mClient.clear();
 
     mPlayer.clear();
 
