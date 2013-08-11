ad372d7a8cc0638e0bec27d221c5c67465c70656
Mingming Yin <mingming@codeaurora.org>
2013-08-11 13:33:41 -0700

    audio: fix for deadlock issue during LPA playback
    EffectModule::setEnable for LPA playback was modified to lock
    AudioFligner::mLock with EffectModule::mLock held.
    And at the same time ~DirectAudioTrack is trying to lock
    EffectModule::mLock with AudioFlinger::mLock held and this
    results in deadlock.
    Fix this issue by reverting previous change and acquire
    locks in certain sequence to avoid deadlock in setEnabled_l
    
    Change-Id: Iea74eb3591a728275c655074a4d2017bd38f99ab

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -646,7 +646,9 @@ status_t AudioFlinger::EffectModule::setEnabled_l(bool enabled)
     */
     if (effectStateChanged && mIsForLPA) {
         sp<ThreadBase> thread = mThread.promote();
+        unlock();//Acquire locks in certain sequence to avoid deadlock
         thread->effectConfigChanged();
+        lock();
     }
 #endif
     return NO_ERROR;
