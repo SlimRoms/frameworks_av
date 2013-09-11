2a747de88a389e54de16b05d6dae6c2ed76664d0
Divya Narayanan Poojary <dnaray@codeaurora.org>
2013-09-11 12:44:58 +0530

    frameworks/av: Fixed framecount in AudioRecord
    - Incorrect frameCount is passed to AudioRecordClientProxy,
      which is used while user side pointer is incremented
      This is causing distortion in SIP call
    - Fix is to use frameCount of the shared block instead of
      frameCount passed by the client
    - InputBytes initialization is also done in AudioFlinger
    
    Change-Id: I4470be49c44e0c86c8a6262f57cabbc609c32045
    Crs-fixed:  525315

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -3835,6 +3835,7 @@ bool AudioFlinger::RecordThread::threadLoop()
                             } else {
                                 readInto = mRsmpInBuffer;
                                 mRsmpInIndex = 0;
+                                InputBytes = mInputBytes;
                             }
                             mBytesRead = mInput->stream->read(mInput->stream, readInto,
                                     InputBytes);
