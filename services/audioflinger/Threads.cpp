9e00d3220c3c03c12441b53d97ca235fe4e1f6e0
Haynes Mathew George <hgeorge@codeaurora.org>
2014-02-11 10:52:29 -0800

    audioflinger: remove unecessary call to HAL
    On a flush, OffloadThread resets its state about HAL being paused.
    For the sake of consistency, it is now expected that after a flush
    as HW is no longer paused, the resume call is unnecessary.
    
    Change-Id: I47d8b685dd23427bb44e24593124b4e42e4a315e

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4292,9 +4292,6 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::OffloadThread::prepareTr
     // if resume is received before pause is executed.
     if (!mStandby && (doHwPause || (mFlushPending && !mHwPaused && (count != 0)))) {
         mOutput->stream->pause(mOutput->stream);
-        if (!doHwPause) {
-            doHwResume = true;
-        }
     }
     if (mFlushPending) {
         flushHw_l();
