b5507dbcfe4ae6e66fd259dd43b93069e5534140
Suresh Choudhary <sureshc@nvidia.com>
2014-03-18 17:24:15 +0530

    libstagefright: Handle unsupported codec metaData
    Modification done in StagefrightMetadataRetriver
    to handle NULL return for meta data if codec is
    unsupported by extractor.
    
    Change-Id: I905cec5ee91b098d99e1935437b562d2d9206381

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -458,6 +458,10 @@ VideoFrame *StagefrightMetadataRetriever::getFrameAtTime(
     for (i = 0; i < n; ++i) {
         sp<MetaData> meta = mExtractor->getTrackMetaData(i);
 
+        if (meta == NULL) {
+            continue;
+        }
+
         const char *mime;
         CHECK(meta->findCString(kKeyMIMEType, &mime));
 
@@ -628,6 +632,10 @@ void StagefrightMetadataRetriever::parseMetaData() {
 
     size_t numTracks = mExtractor->countTracks();
 
+    if (numTracks == 0) {      //If no tracks available, corrupt or not valid stream
+        return;
+    }
+
     char tmp[32];
     sprintf(tmp, "%zu", numTracks);
 
@@ -651,6 +659,9 @@ void StagefrightMetadataRetriever::parseMetaData() {
     String8 timedTextLang;
     for (size_t i = 0; i < numTracks; ++i) {
         sp<MetaData> trackMeta = mExtractor->getTrackMetaData(i);
+        if (trackMeta == NULL) {
+            continue;
+        }
 
         int64_t durationUs;
         if (trackMeta->findInt64(kKeyDuration, &durationUs)) {
