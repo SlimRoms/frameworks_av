45d18db23e33372b9d67baf313d5ee5b39a32fb2
Lars Greiss <kufikugel@googlemail.com>
2014-06-08 14:48:07 +0200

    Merge tag 'android-4.4.3_r1' into HEAD
    Android 4.4.3 release 1
    
    Change-Id: I389b09d150f725e0f49a9d8706838455314b7904

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -2840,9 +2840,6 @@ void OMXCodec::on_message(const omx_message &msg) {
                     ATRACE_INT("Output Buffers with OMX client",
                             countBuffersWeOwn(mPortBuffers[kPortIndexOutput]));
                 }
-                if (mIsEncoder) {
-                    sched_yield();
-                }
             }
 
             ATRACE_END();
