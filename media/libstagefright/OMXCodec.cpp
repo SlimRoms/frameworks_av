b0e920ce5727f7c28f8c6c61e8b0ceb7ae21c716
Steve Kondik <shade@chemlab.org>
2014-08-07 17:00:57 -0700

    stagefright: Improve 24-bit support
     * Cleanup and fixes to allow FFMPEG codecs to output raw 24-bit PCM
    
    Change-Id: Ideeb7cd7c6340f8054f99272796032939b561f81

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -4482,7 +4482,7 @@ status_t OMXCodec::setFLACFormat(const sp<MetaData> &meta)
 
     CHECK(meta->findInt32(kKeyChannelCount, &numChannels));
     CHECK(meta->findInt32(kKeySampleRate, &sampleRate));
-    //CHECK(meta->findInt32(kKeyBitspersample, &bitsPerSample));
+    CHECK(meta->findInt32(kKeySampleBits, &bitsPerSample));
 
     CODEC_LOGV("Channels: %d, SampleRate: %d",
             numChannels, sampleRate);
@@ -4585,7 +4585,7 @@ status_t OMXCodec::setAPEFormat(const sp<MetaData> &meta)
 
     CHECK(meta->findInt32(kKeyChannelCount, &numChannels));
     CHECK(meta->findInt32(kKeySampleRate, &sampleRate));
-    CHECK(meta->findInt32(kKeyBitspersample, &bitsPerSample));
+    CHECK(meta->findInt32(kKeySampleBits, &bitsPerSample));
 
     CODEC_LOGV("Channels:%d, SampleRate:%d, bitsPerSample:%d",
             numChannels, sampleRate, bitsPerSample);
@@ -4662,7 +4662,7 @@ status_t OMXCodec::setFFmpegAudioFormat(const sp<MetaData> &meta)
     CHECK(meta->findInt32(kKeyCodecId, &codec_id));
     CHECK(meta->findInt32(kKeyChannelCount, &numChannels));
     CHECK(meta->findInt32(kKeyBitRate, &bitRate));
-    CHECK(meta->findInt32(kKeyBitspersample, &bitsPerSample));
+    CHECK(meta->findInt32(kKeySampleBits, &bitsPerSample));
     CHECK(meta->findInt32(kKeySampleRate, &sampleRate));
     CHECK(meta->findInt32(kKeyBlockAlign, &blockAlign));
     CHECK(meta->findInt32(kKeySampleFormat, &sampleFormat));
@@ -4685,6 +4685,7 @@ status_t OMXCodec::setFFmpegAudioFormat(const sp<MetaData> &meta)
 
     err = mOMX->setParameter(
             mNode, OMX_IndexParamAudioFFmpeg, &param, sizeof(param));
+
     return err;
 }
 
