5e68c61ec38204aa32a622c84ecc96c89b5e5964
Meticulus <theonejohnnyd@gmail.com>
2014-11-05 22:57:06 +0100

    STE Multimedia Fix
    Change-Id: I41b1993023dcdae651a2171208524018e2381bd8

--- include/media/stagefright/MediaDefs.h
+++ include/media/stagefright/MediaDefs.h
@@ -48,6 +48,9 @@ extern const char *MEDIA_MIMETYPE_VIDEO_HEVC;
 extern const char *MEDIA_MIMETYPE_VIDEO_MPEG4;
 extern const char *MEDIA_MIMETYPE_VIDEO_MPEG4_DP;
 extern const char *MEDIA_MIMETYPE_VIDEO_H263;
+#ifdef STE_HARDWARE
+extern const char *MEDIA_MIMETYPE_VIDEO_H263_SW;
+#endif
 extern const char *MEDIA_MIMETYPE_VIDEO_MPEG2;
 extern const char *MEDIA_MIMETYPE_VIDEO_RAW;
 
