8925a1e3ff27334ac6a426e27a954d2515a7d19d
Naresh Tanniru <ntanniru@codeaurora.org>
2014-11-06 17:21:58 +0530

    Revert "libstagefright: Add support for HW AAC decoder"
    - This reverts commit 146757db52a6531b90404b478d12f277f9cd39c9.
    
    - On PDT tetsing crashes observed due to improper selection
      of multi aac decoder without setting system property
    
    - As multi aac decoder using only for ISDBT case, reverting
      this gerrit to unblock PDT testing till root cause for crash
      is identified.
    
    Change-Id: I1c195bb9150199ae71c686081d75bbb7653fa8fe

--- media/libstagefright/ExtendedCodec.cpp
+++ media/libstagefright/ExtendedCodec.cpp
@@ -1139,6 +1139,18 @@ status_t ExtendedCodec::setAMRWBPLUSFormat(
     return err;
 }
 
+bool ExtendedCodec::useHWAACDecoder(const char *mime) {
+    char value[PROPERTY_VALUE_MAX] = {0};
+    int aaccodectype = 0;
+    aaccodectype = property_get("media.aaccodectype", value, NULL);
+    if (aaccodectype && !strncmp("0", value, 1) &&
+        !strcasecmp(mime, MEDIA_MIMETYPE_AUDIO_AAC)) {
+        ALOGI("Using Hardware AAC Decoder");
+        return true;
+    }
+    return false;
+}
+
 bool ExtendedCodec::isSourcePauseRequired(const char *componentName) {
     /* pause is required for hardware component to release adsp resources */
     if (!strncmp(componentName, "OMX.qcom.", 9)) {
@@ -1420,6 +1432,11 @@ namespace android {
         ARG_TOUCH(flags);
         ARG_TOUCH(nodeID);
         ARG_TOUCH(componentName);
+    }
+
+    bool ExtendedCodec::useHWAACDecoder(const char *mime) {
+        ARG_TOUCH(mime);
+        return false;
     }
 
     void ExtendedCodec::enableSmoothStreaming(
