c5f65d6ad4de69a51213b5fd1ed6e43b9f3046c4
Jia Meng <jmeng@codeaurora.org>
2014-11-12 11:24:47 +0800

    stagefright: Clear mNoMoreOutputData flag during seeking
    For some 3rd party players, stop won't be called when playing
    to the end. If seeking happens after that, we need to clear
    this flag to make it be ready for subsequence data flow.
    
    Change-Id: I9b2d18d03225420a45821ba7536cdc91d52b1312
    CRs-Fixed: 755988

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -2548,6 +2548,7 @@ void OMXCodec::onCmdComplete(OMX_COMMANDTYPE cmd, OMX_U32 data) {
 
                     // We implicitly resume pulling on our upstream source.
                     mPaused = false;
+                    mNoMoreOutputData = false;
 
                     drainInputBuffers();
                     fillOutputBuffers();
