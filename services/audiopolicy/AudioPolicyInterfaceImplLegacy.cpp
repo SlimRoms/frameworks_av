697764f852a7fe59df8630c07f1c0e0b709e73b8
Stefan Berger <s.berger81@gmail.com>
2014-12-26 21:45:47 +0100

    Revert: STE: volume index can't be changed for each audio device
    Change-Id: Icc837fa7c3a74bb0570b5218bb45ab395f597096

--- services/audiopolicy/AudioPolicyInterfaceImplLegacy.cpp
+++ services/audiopolicy/AudioPolicyInterfaceImplLegacy.cpp
@@ -368,17 +368,13 @@ status_t AudioPolicyService::setStreamVolumeIndex(audio_stream_type_t stream,
         return BAD_VALUE;
     }
     Mutex::Autolock _l(mLock);
-#ifndef STE_HARDWARE
     if (mpAudioPolicy->set_stream_volume_index_for_device) {
         return mpAudioPolicy->set_stream_volume_index_for_device(mpAudioPolicy,
                                                                 stream,
                                                                 index,
                                                                 device);
-    } else 
-#endif
-    {
-        return mpAudioPolicy->set_stream_volume_index(mpAudioPolicy, stream, index);
     }
+    return mpAudioPolicy->set_stream_volume_index(mpAudioPolicy, stream, index);
 }
 
 status_t AudioPolicyService::getStreamVolumeIndex(audio_stream_type_t stream,
@@ -392,17 +388,13 @@ status_t AudioPolicyService::getStreamVolumeIndex(audio_stream_type_t stream,
         return BAD_VALUE;
     }
     Mutex::Autolock _l(mLock);
-#ifndef STE_HARDWARE
     if (mpAudioPolicy->get_stream_volume_index_for_device) {
         return mpAudioPolicy->get_stream_volume_index_for_device(mpAudioPolicy,
                                                                 stream,
                                                                 index,
                                                                 device);
-    } else 
-#endif
-    {
-        return mpAudioPolicy->get_stream_volume_index(mpAudioPolicy, stream, index);
     }
+    return mpAudioPolicy->get_stream_volume_index(mpAudioPolicy, stream, index);
 }
 
 uint32_t AudioPolicyService::getStrategyForStream(audio_stream_type_t stream)
