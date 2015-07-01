339e69b04552cc08a4501ef6eee1b8bb461792a4
Marco Nelissen <marcone@google.com>
2015-07-01 13:05:50 -0700

    Check buffer size before using it
    CVE-2015-3873
    
    Bug: 21814993
    Change-Id: Idaac61b4b9f4058b94e84093644593ba315d72ff
    (cherry picked from commit c1a104aaad2d84a57bf5d87dd030d2bef56bf541)

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -3307,6 +3307,10 @@ status_t MPEG4Source::read(
             CHECK(mBuffer == NULL);
             return err;
         }
+        if (size > mBuffer->size()) {
+            ALOGE("buffer too small: %zu > %zu", size, mBuffer->size());
+            return ERROR_BUFFER_TOO_SMALL;
+        }
     }
 
     if (!mIsAVC || mWantsNALFragments) {
@@ -3551,6 +3555,10 @@ status_t MPEG4Source::fragmentedRead(
             CHECK(mBuffer == NULL);
             ALOGV("acquire_buffer returned %d", err);
             return err;
+        }
+        if (size > mBuffer->size()) {
+            ALOGE("buffer too small: %zu > %zu", size, mBuffer->size());
+            return ERROR_BUFFER_TOO_SMALL;
         }
     }
 
