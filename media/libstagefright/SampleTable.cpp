CyanogenMod/android_frameworks_av

Fixed comparison on 64 bit system
Fixed comparison in setSampleToChunkParams which
return false since SIZE_MAX in 64 bit system is
18446744073709551615, but off64_t is signed.

Change-Id: Idba6b94e2dbdcc34ced98a5f926f1b5e0b6cf5cb

media/libstagefright/SampleTable.cpp
@@ -246,7 +246,7 @@ status_t SampleTable::setSampleToChunkParams(
     for (uint32_t i = 0; i < mNumSampleToChunkOffsets; ++i) {
         uint8_t buffer[12];
 
-        if ((off64_t)(SIZE_MAX - 8 - (i * 12)) < mSampleToChunkOffset) {
+        if ((off64_t)(UINT32_MAX - 8 - (i * 12)) < mSampleToChunkOffset) {
             return ERROR_MALFORMED;
         }
 

SlimRoms/frameworks_av

media/libstagefright/SampleTable.cpp
@@ -246,7 +246,7 @@ status_t SampleTable::setSampleToChunkParams(
     for (uint32_t i = 0; i < mNumSampleToChunkOffsets; ++i) {
         uint8_t buffer[12];
 
-        if ((off64_t)(SIZE_MAX - 8 - (i * 12)) < mSampleToChunkOffset) {
+        if ((off64_t)(INT64_MAX - 8 - (i * 12)) < mSampleToChunkOffset) {
             return ERROR_MALFORMED;
         }
 