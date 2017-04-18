3b5055b31ce9ff96ee497641e3236b5be2ef3af3
Dhananjay Kumar <dhakumar@codeaurora.org>
2017-04-18 16:29:02 +0530

    frameworks/av: follow-up change for enabling audio extended features
    -Enable 24 bit no-compress offload playback
    -Add support for 6 channel recording.
    
    AVCustomizations: Enable 16 and 24 bit PCM offload
    Change-Id: I3363140fad441a7746884076c40b46e777f2e06e
    audio: add support for 5.1 channel recording
    Change-Id: If060fffb2e198f516f40e85390489de2108be5d1
    
    Change-Id: I7a3c2d8c5cb7a5f4858de828c545674581eb43a7

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -34,6 +34,9 @@
 
 #include <inttypes.h>
 
+#include "mediaplayerservice/AVNuExtensions.h"
+#include "stagefright/AVExtensions.h"
+
 namespace android {
 
 /*
@@ -1974,7 +1977,7 @@ status_t NuPlayer::Renderer::onOpenAudioSink(
         const PcmInfo info = {
                 (audio_channel_mask_t)channelMask,
                 (audio_output_flags_t)pcmFlags,
-                AUDIO_FORMAT_PCM_16_BIT, // TODO: change to audioFormat
+                AVNuUtils::get()->getPCMFormat(format),
                 numChannels,
                 sampleRate
         };
@@ -2013,7 +2016,7 @@ status_t NuPlayer::Renderer::onOpenAudioSink(
                     sampleRate,
                     numChannels,
                     (audio_channel_mask_t)channelMask,
-                    AUDIO_FORMAT_PCM_16_BIT,
+                    AVNuUtils::get()->getPCMFormat(format),
                     0 /* bufferCount - unused */,
                     mUseAudioCallback ? &NuPlayer::Renderer::AudioSinkCallback : NULL,
                     mUseAudioCallback ? this : NULL,
