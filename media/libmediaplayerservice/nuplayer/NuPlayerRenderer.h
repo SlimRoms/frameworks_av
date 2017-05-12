07bb6921fae2f708de28fde23d0aeadadc9a6a60
Dhananjay Kumar <dhakumar@codeaurora.org>
2017-05-12 16:49:43 +0530

    nuplayer: extend nuplayer renderer to handle gapless switch for direct pcm
    Make NuPlayer::Renderer::onOpenAudioSink virtual to extend it in
    ExtendedNuPlayerRenderer, this is required to handle gapless switch
    for direct pcm playbacks.
    
    Change-Id: I4967992ab1ab4f30caf24e530f32978f40053fb1

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.h
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.h
@@ -265,7 +265,7 @@ private:
     int32_t getDrainGeneration(bool audio);
     bool getSyncQueues();
     void onAudioTearDown(AudioTearDownReason reason);
-    status_t onOpenAudioSink(
+    virtual status_t onOpenAudioSink(
             const sp<AMessage> &format,
             bool offloadOnly,
             bool hasVideo,
