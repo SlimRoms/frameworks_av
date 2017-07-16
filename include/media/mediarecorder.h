79ec86330368e3639bc09df7f2f6b5871990550c
Josue Rivera <prbassplayer@gmail.com>
2017-07-16 13:29:50 -0400

    Revert "MediaProfiles: Enhance profiles for extended usecases"
    This reverts commit 8e18f0cfe58027b6159201b4543ac0e98878413a.
    
    Change-Id: I720993f2f4c5967a40cfe360addcbf57e5820fda
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- include/media/mediarecorder.h
+++ include/media/mediarecorder.h
@@ -1,7 +1,4 @@
 /*
- ** Copyright (c) 2014, The Linux Foundation. All rights reserved.
- ** Not a Contribution.
- **
  ** Copyright (C) 2008 The Android Open Source Project
  **
  ** Licensed under the Apache License, Version 2.0 (the "License");
